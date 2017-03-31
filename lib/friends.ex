f0f056934aa7154ff6ff79fa395366355953c6a4
Wu Qing <wiserfirst@gmail.com>
2017-03-31 15:23:32 +1100

    add ecto config and repo

--- lib/friends.ex
+++ lib/friends.ex
@@ -10,6 +10,7 @@ defmodule Friends do
     children = [
       # Starts a worker by calling: Friends.Worker.start_link(arg1, arg2, arg3)
       # worker(Friends.Worker, [arg1, arg2, arg3]),
+      supervisor(Friends.Repo, []),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
